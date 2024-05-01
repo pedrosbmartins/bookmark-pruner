93cf26e334601ba7eeace1d460b21192e3f3112a
Pedro Martins <pedrosbarbosamartins@gmail.com>
2024-05-01 13:00:11 -0300

    Add simple popup content

--- popup.tsx
+++ popup.tsx
@@ -1,24 +1,40 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+
+import { sendToBackground } from "@plasmohq/messaging"
+
+import "./style.css"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [mainShortcut, setMainShortcut] = useState("No shortcut")
+
+  useEffect(() => {
+    chrome.commands.getAll((commands) => {
+      for (let { name, shortcut } of commands) {
+        if (name === "start-main-flow") {
+          setMainShortcut(shortcut)
+        }
+      }
+    })
+  }, [])
 
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div className="w-[200px] p-4">
+      <h2 className="text-lg">Ditch me</h2>
+      <div className="mt-2 text-[1.2em]">
+        <button
+          className="underline my-1 block"
+          onClick={async () => {
+            await sendToBackground({ name: "start" })
+          }}>
+          Start
+        </button>
+        <span className="my-1 text-[0.9em]">{mainShortcut}</span>
+        <div className="my-2">
+          <h3 className="font-bold">Root folder</h3>
+          <span className="block font-mono">Other bookmarks</span>
+          <button className="underline">Change</button>
+        </div>
+      </div>
     </div>
   )
 }
