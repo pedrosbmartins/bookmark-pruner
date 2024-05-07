95d471cebe247b07cc4126ed282d002e943c5ded
Pedro Martins <pedrosbarbosamartins@gmail.com>
2024-05-06 21:29:58 -0300

    Improve bookmark age label

--- src/content.tsx
+++ src/content.tsx
@@ -6,6 +6,8 @@ import { useEffect, useState } from "react"
 
 import { sendToBackground } from "@plasmohq/messaging"
 
+import type { Bookmark } from "~background/messages/bookmark"
+
 export const config: PlasmoCSConfig = {
   matches: ["<all_urls>"],
   all_frames: true
@@ -61,14 +63,19 @@ const CustomButton = () => {
     async function fetchActiveBookmark() {
       const response = await sendToBackground({ name: "bookmark" })
       if (!response || !response.bookmark) return
-      setTargetURL(response.bookmark.url)
-      const now = new Date().getTime()
-      const added = new Date(response.bookmark.dateAdded).getTime()
-      const differenceInDays = (now - added) / 1000 / 60 / 60 / 24
-      setDaysSinceAdded(differenceInDays)
+      onBookmarkLoaded(response.bookmark)
     }
+
     fetchActiveBookmark()
   }, [])
+
+  function onBookmarkLoaded(bookmark: Bookmark) {
+    setTargetURL(bookmark.url)
+    const now = new Date().getTime()
+    const added = new Date(bookmark.dateAdded).getTime()
+    const differenceInDays = (now - added) / 1000 / 60 / 60 / 24
+    setDaysSinceAdded(differenceInDays)
+  }
 
   if (!targetURL || targetURL !== window.location.href) {
     return null
@@ -95,7 +102,7 @@ const CustomButton = () => {
           <div
             className={`${colors[ageLevel]} w-2 h-2 rounded-[4px] mr-2`}></div>
           <span className="text-[0.9em] text-stone-500">
-            {Math.floor(daysSinceAdded)} days old
+            {ageLabel(daysSinceAdded)}
           </span>
         </div>
       </div>
@@ -113,6 +120,30 @@ const CustomButton = () => {
       </button>
     </div>
   )
+}
+
+function ageLabel(daysSinceAdded: number) {
+  const config = ageLabelConfig(daysSinceAdded)
+  const value = Math.floor(daysSinceAdded / config.divider)
+  const label = value === 1 ? config.label : `${config.label}s`
+  return `${value} ${label} old`
+}
+
+// prettier-ignore
+function ageLabelConfig(daysSinceAdded: number) {
+  switch (true) {
+    case daysSinceAdded <  14: return labelConfig.days
+    case daysSinceAdded <  30: return labelConfig.weeks
+    case daysSinceAdded < 365: return labelConfig.months
+    default:                   return labelConfig.years
+  }
+}
+
+const labelConfig = {
+  days: { divider: 1, label: "day" },
+  weeks: { divider: 7, label: "week" },
+  months: { divider: 30, label: "month" },
+  years: { divider: 365, label: "year" }
 }
 
 export default CustomButton
