283181be9204d9c1a590e4fc75221fdce8f4e3ca
Pedro Martins <pedrosbarbosamartins@gmail.com>
2024-10-05 19:08:32 +0200

    Detect chrome vs firefox when selecting default bookmark node ID

--- src/core/store.ts
+++ src/core/store.ts
@@ -8,11 +8,24 @@ const keys = {
   ROOT_BOOKMARK_NODE_ID: "rootBookmarkNodeId"
 }
 
-const DEFAULT_BOOKMARK_NODE_ID = "2"
+// @todo: move to separate file
+let DEFAULT_BOOKMARK_NODE_ID: string | undefined
+const DEFAULT_CHROME_BOOKMARK_NODE_ID = "2"
+const DEFAULT_FIREFOX_BOOKMARK_NODE_ID = "menu________"
 
 export async function getRootBookmarkNodeId() {
   const id = await storage.get(keys.ROOT_BOOKMARK_NODE_ID)
-  return id ?? DEFAULT_BOOKMARK_NODE_ID
+  if (DEFAULT_BOOKMARK_NODE_ID === undefined) {
+    try {
+      await chrome.bookmarks.getSubTree(DEFAULT_FIREFOX_BOOKMARK_NODE_ID)
+      console.info("using firefox default node ID")
+      DEFAULT_BOOKMARK_NODE_ID = DEFAULT_FIREFOX_BOOKMARK_NODE_ID
+    } catch (e) {
+      console.info("using chrome default node ID")
+      DEFAULT_BOOKMARK_NODE_ID = DEFAULT_CHROME_BOOKMARK_NODE_ID
+    }
+  }
+  return id ?? (DEFAULT_BOOKMARK_NODE_ID as string)
 }
 
 export async function setRootBookmarkNodeId(id: string) {
